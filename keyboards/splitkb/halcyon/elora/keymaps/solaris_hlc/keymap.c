fb5c571e4f48f23f27db1dfb651aafe6094b9c7b
Marco Stambor <mail@marco-stambor.de>
2025-02-16 21:53:54 +0100

    Syntax?

--- keyboards/splitkb/halcyon/elora/keymaps/solaris_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/solaris_hlc/keymap.c
@@ -283,7 +283,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 void module_post_init_user(void) {
-    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_GRADIENT);
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_GRADIENT);
     rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
     rgb_matrix_sethsv_noeeprom(HSV_WHITE);
 }
