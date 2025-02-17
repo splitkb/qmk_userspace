cfbae67ddd88635983c4bdff30fb4bec086f71f0
Marco Stambor <marco.stambor@empowersuite.com>
2025-02-17 09:46:20 +0100

    use user function

--- keyboards/splitkb/halcyon/elora/keymaps/solaris_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/solaris_hlc/keymap.c
@@ -283,7 +283,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 #ifdef ENCODER_ENABLE
-bool encoder_update_kb(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) {
         return false;
     }
