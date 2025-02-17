03977a36120aec09456801229a90b4f8a583f743
Marco Stambor <marco.stambor@empowersuite.com>
2025-02-17 09:42:01 +0100

    encoder

--- keyboards/splitkb/halcyon/elora/keymaps/solaris_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/solaris_hlc/keymap.c
@@ -281,3 +281,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //     ),
 //
 };
+
+#ifdef ENCODER_ENABLE
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) {
+        return false;
+    }
+    if (index == 0) {
+        // Volume control
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } else if (index == 1) {
+      if (clockwise) {
+          tap_code(KC_VOLU);
+      } else {
+          tap_code(KC_VOLD);
+      }
+    }
+    return false;
+}
+#endif
