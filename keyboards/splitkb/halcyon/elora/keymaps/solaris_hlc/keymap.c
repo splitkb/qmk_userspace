bf574f7ff4f7043088b087adbb4b32cb37cc80ba
Marco Stambor <marco.stambor@empowersuite.com>
2025-02-18 12:37:47 +0100

    Fix missing return statement

--- keyboards/splitkb/halcyon/elora/keymaps/solaris_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/solaris_hlc/keymap.c
@@ -289,6 +289,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     isMac = IS_LAYER_ON_STATE(state, _MAC);
+    return state;
 }
 
 // increase the tapping terms for the pinkys slightly,
